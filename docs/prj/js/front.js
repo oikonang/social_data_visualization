8d94b40e960ef98515ea74a3714f30bc3e3bd152
Luca Cambiaghi <lc.cambiaghi@gmail.com>
2017-05-04 13:35:20 +0200

    Small bugfixes, website now is responsive

--- docs/prj/js/front.js
+++ docs/prj/js/front.js
@@ -1,9 +1,9 @@
-$(function () {
+$(function() {
 
     lightbox();
     sticky();
     utils();
-    map();
+    //map();
     demo();
 
 });
@@ -12,7 +12,7 @@ $(function () {
 
 function demo() {
 
-    $("#page").change(function () {
+    $("#page").change(function() {
 
         if ($(this).val() !== '') {
 
@@ -30,7 +30,7 @@ function demo() {
 
 function lightbox() {
 
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
     });
@@ -51,61 +51,63 @@ function sticky() {
  *  map 
  *  =======================================*/
 
-function map() {
+// function map() {
 
-    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
-    map = new GMaps({
-        el: '#map',
-        lat: -12.043333,
-        lng: -77.028333,
-        zoomControl: true,
-        zoomControlOpt: {
-            style: 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        overviewMapControl: false,
-        scrollwheel: false,
-        draggable: false,
-        styles: styles
-    });
 
-    var image = 'img/marker.png';
+//     var styles = [{ "featureType": "landscape", "stylers": [{ "saturation": -100 }, { "lightness": 65 }, { "visibility": "on" }] }, { "featureType": "poi", "stylers": [{ "saturation": -100 }, { "lightness": 51 }, { "visibility": "simplified" }] }, { "featureType": "road.highway", "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }] }, { "featureType": "road.arterial", "stylers": [{ "saturation": -100 }, { "lightness": 30 }, { "visibility": "on" }] }, { "featureType": "road.local", "stylers": [{ "saturation": -100 }, { "lightness": 40 }, { "visibility": "on" }] }, { "featureType": "transit", "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }] }, { "featureType": "administrative.province", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "lightness": -25 }, { "saturation": -100 }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "hue": "#ffff00" }, { "lightness": -25 }, { "saturation": -97 }] }];
+//     var map = new Gmaps({
+//         el: '#map',
+//         lat: -12.043333,
+//         lng: -77.028333,
+//         zoomControl: true,
+//         zoomControlOpt: {
+//             style: 'SMALL',
+//             position: 'TOP_LEFT'
+//         },
+//         panControl: false,
+//         streetViewControl: false,
+//         mapTypeControl: false,
+//         overviewMapControl: false,
+//         scrollwheel: false,
+//         draggable: false,
+//         styles: styles
+//     });
 
-    map.addMarker({
-        lat: -12.043333,
-        lng: -77.028333,
-        icon: image/* ,
-         title: '',
-         infoWindow: {
-         content: '<p>HTML Content</p>'
-         }*/
-    });
-}
+//     var image = 'img/marker.png';
+
+//     map.addMarker({
+//         lat: -12.043333,
+//         lng: -77.028333,
+//         icon: image
+//             /* ,
+//                      title: '',
+//                      infoWindow: {
+//                      content: '<p>HTML Content</p>'
+//                      }*/
+//     });
+// }
 
 function utils() {
 
     /* tooltips */
 
-    $('[data-toggle="tooltip"]').tooltip();
+    //$('[data-toggle="tooltip"]').tooltip();
 
     /* click on the box activates the radio */
 
-    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function (e) {
+    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function(e) {
         var radio = $(this).find(':radio');
         radio.prop('checked', true);
     });
     /* click on the box activates the link in it */
 
-    $('.box.clickable').on('click', function (e) {
+    $('.box.clickable').on('click', function(e) {
 
         window.location = $(this).find('a').attr('href');
     });
     /* external links in new window*/
 
-    $('.external').on('click', function (e) {
+    $('.external').on('click', function(e) {
 
         e.preventDefault();
         window.open($(this).attr("href"));
@@ -114,26 +116,26 @@ function utils() {
 
     /* animated scrolling */
 
-    $('.scroll-to, #navigation a').click(function (event) {
+    $('.scroll-to, #navigation a').click(function(event) {
         event.preventDefault();
         var full_url = this.href;
         var parts = full_url.split("#");
         var trgt = parts[1];
 
-        $('body').scrollTo($('#' + trgt), 800, {offset: -40});
+        $('body').scrollTo($('#' + trgt), 800, { offset: -40 });
 
     });
 
 }
 
-$.fn.alignElementsSameHeight = function () {
-    $('.same-height-row').each(function () {
+$.fn.alignElementsSameHeight = function() {
+    $('.same-height-row').each(function() {
 
         var maxHeight = 0;
         var children = $(this).find('.same-height');
         children.height('auto');
         if ($(window).width() > 768) {
-            children.each(function () {
+            children.each(function() {
                 if ($(this).innerHeight() > maxHeight) {
                     maxHeight = $(this).innerHeight();
                 }
@@ -144,7 +146,7 @@ $.fn.alignElementsSameHeight = function () {
         maxHeight = 0;
         children = $(this).find('.same-height-always');
         children.height('auto');
-        children.each(function () {
+        children.each(function() {
             if ($(this).innerHeight() > maxHeight) {
                 maxHeight = $(this).innerHeight();
             }
@@ -153,7 +155,10 @@ $.fn.alignElementsSameHeight = function () {
     });
 }
 
-$(window).load(function () {
+$(window).load(function() {
+
+    var windowHeight;
+    var windowWidth;
 
     windowWidth = $(window).width();
     windowHeight = $(window).height();
@@ -161,13 +166,14 @@ $(window).load(function () {
     $(this).alignElementsSameHeight();
 
 });
-$(window).resize(function () {
+$(window).resize(function() {
+    var windowWidth;
 
     newWindowWidth = $(window).width();
     newWindowHeight = $(window).height();
 
     if (windowWidth !== newWindowWidth) {
-        setTimeout(function () {
+        setTimeout(function() {
             $(this).alignElementsSameHeight();
         }, 100);
         windowWidth = newWindowWidth;
